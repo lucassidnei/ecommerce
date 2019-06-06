b0084b593807acfcfc32dda634721d18571f8ef2
Lucas Sidnei <nerosidnei@gmail.com>
2019-06-06 16:17:46 -0300

    registro

--- index.php
+++ index.php
@@ -64,6 +64,104 @@ $app->get('/admin/logout', function(){
 
 });
 
+$app->get("/admin/user", function(){
+
+User::verifyLogin();
+
+User::listAll();
+
+$page = new PageAdmin();
+
+$page->setTpl("users", array(
+
+	"user"=>$user
+
+));
+
+});
+
+$app->get("/admin/user/create", function(){
+
+User::verifyLogin();
+
+$page = new PageAdmin();
+
+$page->setTpl("users-create");
+
+});
+
+$app->get("/admin/user/:iduser/delete", function($iduser){
+
+User::verifyLogin();
+
+});
+
+
+$app->get("/admin/user/:iduser", function($iduser){
+
+User::verifyLogin();
+
+$user = new User();
+
+$user->get((int)$iduser);
+
+$page = new PageAdmin();
+
+$page->setTpl("users-update",array(
+
+	"user"=>$user->getValues()
+
+	));
+
+});
+
+
+$app->post("/admin/users/create", function () {
+
+ 	User::verifyLogin();
+
+	$user = new User();
+
+ 	$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
+
+ 	$_POST['despassword'] = password_hash($_POST["despassword"], PASSWORD_DEFAULT, [
+
+ 		"cost"=>12
+
+ 	]);
+
+ 	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+ 	exit;
+
+	});
+
+	$app->post("/admin/user/:iduser", function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
+
+	});
+
+
+
+
+
 $app->run();
 
  ?>
