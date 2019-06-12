5224f562bbeeeebb19066cc60887a03291633d06
Lucas Sidnei <nerosidnei@gmail.com>
2019-06-12 13:34:03 -0300

    Site Categoria

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -24,7 +24,8 @@ class Category extends Model {
 			":idcategory"=>$this->getidcategory(),
 			":descategory"=>$this->getdescategory()
 		));
-		$this->setData($results[0]);
+        $this->setData($results[0]);
+        Category::updateFile();
     }
     
 
@@ -44,6 +45,19 @@ class Category extends Model {
         $sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory ", [
             ':idcategory'=>$this->getidcategory()
         ]);
+
+        Category::updateFile();
+    }
+
+
+    public static function updateFile(){
+
+        $categories = Category::listAll();
+        $html = [];
+        foreach($categories as $row){
+            array_push($html, '<li><a href="/categories/' . $row['idcategory'] . '">' . $row['descategory'] . '</a></li>');
+        }
+        file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
     }
 
 }
